df80cdae67758f0c009a8a3073489e79e737d9fb
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-05 22:52:12 +0530

    global object

--- test.js
+++ test.js
@@ -1,9 +1,25 @@
-var name = "jash";
+// // node.js basics
+// var name = "jash";
 // console.log(name);
 
-const greeting = (name) => {
-  console.log(`hello,${name}`);
-};
+// const greeting = (name) => {
+//   console.log(`hello,${name}`);
+// };
 
-greeting(name);
-greeting("shani");
+// greeting(name);
+// greeting("shani");
+
+// // global object
+// console.log(global);
+
+global.setTimeout(() => {
+  console.log("hello shani");
+  clearInterval(int);
+}, 3000);
+
+const int = setInterval(() => {
+  console.log("hello this is interval");
+}, 1000);
+
+console.log(__dirname);
+console.log(__filename);
