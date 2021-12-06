30728c90437a4391b54568f8a51922e6557f9b6b
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-06 21:16:38 +0530

    modules and require

---
+++ modules.js
@@ -0,0 +1,8 @@
+// const ppl = require("./people");
+
+const { people, ages } = require("./people");
+console.log(people, ages);
+
+// buildin modules
+const os = require("os");
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ["shani", "jash", "akki"];
+const ages = [22, 1, 223];
+
+module.exports = {
+  people,
+  ages,
+};
