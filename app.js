b49e8424a1a9af6b353cc15602025daa8059c3c0
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-07 20:30:04 +0530

    ejs view engine

--- app.js
+++ app.js
@@ -3,24 +3,51 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listening for requests
 app.listen(3000);
 
+// app.get("/", (req, res) => {
+//   //   res.send("<h1>SHANI</h1>");
+//   res.sendFile("./views/index.html", { root: __dirname });
+// });
+
 app.get("/", (req, res) => {
   //   res.send("<h1>SHANI</h1>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
+
+// app.get("/about", (req, res) => {
+//   //   res.send("<h1>SHANI</h1>");
+//   res.sendFile("./views/about.html", { root: __dirname });
+// });
+
+// // redirects
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
+
 app.get("/about", (req, res) => {
   //   res.send("<h1>SHANI</h1>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
+});
+
+app.get("/about", (req, res) => {
+  //   res.send("<h1>SHANI</h1>");
+  res.render("about");
 });
 
 // redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 error
+// app.use((req, res) => {
+//   res.status(404).sendFile("./views/404.html", { root: __dirname });
+// });
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
