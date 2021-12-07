325c223661d9269abca866edd66e37fc935082af
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-07 21:57:36 +0530

    passing data to views

--- app.js
+++ app.js
@@ -16,7 +16,21 @@ app.listen(3000);
 
 app.get("/", (req, res) => {
   //   res.send("<h1>SHANI</h1>");
-  res.render("index");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 // app.get("/about", (req, res) => {
@@ -31,17 +45,17 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   //   res.send("<h1>SHANI</h1>");
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/about", (req, res) => {
   //   res.send("<h1>SHANI</h1>");
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 // redirects
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "New Blog" });
 });
 
 // 404 error
@@ -49,5 +63,5 @@ app.get("/blogs/create", (req, res) => {
 //   res.status(404).sendFile("./views/404.html", { root: __dirname });
 // });
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Not Found!" });
 });
