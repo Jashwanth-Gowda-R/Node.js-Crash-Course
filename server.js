463ccab1a4815e52aa8e7f0935273c90a5faf4b3
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-07 13:03:26 +0530

    req aand res obj

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("request made");
+  console.log(req.url, req.method);
+
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello Shanis");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
