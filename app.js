195926e5bcb6428cc071c827526d0cbaf7acd019
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-08 15:04:33 +0530

    getting data from db and displaying in home page using mongoose

--- app.js
+++ app.js
@@ -29,47 +29,13 @@ app.set("view engine", "ejs");
 
 // middleware for static files
 app.use(express.static("public"));
-// app.use(express.static("public"));
+
 // middleware for logging
 app.use(morgan("dev"));
 
-// app.get("/", (req, res) => {
-//   //   res.send("<h1>SHANI</h1>");
-//   res.sendFile("./views/index.html", { root: __dirname });
-// });
-
+// routes
 app.get("/", (req, res) => {
-  //   res.send("<h1>SHANI</h1>");
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
-});
-
-// app.get("/about", (req, res) => {
-//   //   res.send("<h1>SHANI</h1>");
-//   res.sendFile("./views/about.html", { root: __dirname });
-// });
-
-// // redirects
-// app.get("/about-us", (req, res) => {
-//   res.redirect("/about");
-// });
-
-app.get("/about", (req, res) => {
-  //   res.send("<h1>SHANI</h1>");
-  res.render("about", { title: "About" });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
@@ -77,41 +43,28 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "New Blog" });
+app.get("/about-us", (req, res) => {
+  //   res.send("<h1>SHANI</h1>");
+  res.redirect("/about");
 });
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "my 2nd blog post",
-    snippet: "Dolor eos dolor duo sit voluptua sea, magna sanctus justo.",
-    body: "Est sea nonumy duo sit invidunt justo sit clita. Elitr vero lorem aliquyam sadipscing, dolor elitr stet et erat eirmod eos amet clita duo. Sea.",
-  });
-  blog
-    .save()
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
     .then((result) => {
-      res.send(result);
+      res.render("index", { title: "All Blogs", blogs: result });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-// retrive all blog items
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "New Blog" });
 });
 
-// 404 error
-// app.use((req, res) => {
-//   res.status(404).sendFile("./views/404.html", { root: __dirname });
-// });
+// middleware for 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "Not Found!" });
 });
